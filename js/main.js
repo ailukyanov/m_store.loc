3c1eae522a3d7675c4d757b540f647fd2b92e326
salexseen <salexseen@gmail.com>
2020-05-11 13:06:57 +0300

    update

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function() {
+  /* Ползунок */
+  var sliderRange = $("#slider-range");
+  var amount = $("#amount");
+
+  sliderRange.slider({
+    range: true,
+    min: 0,
+    max: 999,
+    values: [ 0, 999 ],
+    slide: function( event, ui ) {
+      $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+    }
+  });
+  amount.val("$" + sliderRange.slider( "values", 0 ) + " - $" + sliderRange.slider( "values", 1 ) );
+});
+
+$(function() {
+  /* Селект */
+  cuSel({
+    changedEl: "select",
+    visRows: 5,
+    scrollArrows: true
+  });
+});
